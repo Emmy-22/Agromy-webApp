7c6826681357ba1bb504a8ed0fb808917c2d2d03
Akintobi Abibat Olamide <akintobiabibat@yahoo.com>
2025-09-29 08:45:19 +0000

    added joinus section

--- Agromy-webApp/src/App.jsx
+++ Agromy-webApp/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import JoinUS from './components/JoinUs.jsx';
 import OtpVerification from './components/OtpVerification.jsx';
 
 function App() {
 
   return (
     <>
-    
+    <JoinUS />
     <OtpVerification />
     </>
   );
