be68873d0be6645a8032768325675380af76e8c9
elizabethakinsiku <elizabethakinsiku@gmail.com>
2025-10-03 23:16:45 +0100

    Login page

--- Agromy-webApp/src/App.jsx
+++ Agromy-webApp/src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css'
-import OtpVerification from './components/OtpVerification.jsx';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './components/Login.jsx';
 
 function App() {
 
   return (
-    <>
-    
-    <OtpVerification />
-    </>
+    <Router>
+      <Login />
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
